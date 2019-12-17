df3f599583b121afc9c75f9ebe4991dc5e0d9280
Brian.Chu <chuchengpeng0119@163.com>
2019-12-17 14:07:05 +0800

    Initial Commit

---
+++ CCPObfuscation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CCPObfuscation
+//
+//  Created by 储诚鹏 on 2019/12/17.
+//  Copyright © 2019 储诚鹏. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CCPObfuscation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CCPObfuscation
+//
+//  Created by 储诚鹏 on 2019/12/17.
+//  Copyright © 2019 储诚鹏. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
